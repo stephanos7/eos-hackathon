743b7c2996c3c02cc1c3cb26c93bbbe175248430
hamid.adelyar <hamidadelyar@gmail.com>
2018-09-22 23:28:22 +0100

    Completed splash screen

--- kudos_app/src/components/UserHome/index.js
+++ kudos_app/src/components/UserHome/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./styles.css";
 import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
 
 class Main extends Component {
   render() {
@@ -8,6 +9,32 @@ class Main extends Component {
       <main>
         <div className="splash-container">
           <div className="logo" />
+        </div>
+        <div className="welcome-user-container">
+          <h2>Kudos</h2>
+          <p>
+            Welcome to KUDOS, where you have full control of your identity
+            verification
+          </p>
+          <p>Tell me what authentication are you preparing for?</p>
+
+          <div className="options-container">
+            <Button variant="contained" className="buttons">
+              Mortgage
+            </Button>
+
+            <Button variant="contained" className="buttons">
+              Rental
+            </Button>
+
+            <Button variant="contained" className="buttons">
+              Job
+            </Button>
+
+            <Button variant="contained" className="buttons">
+              Security
+            </Button>
+          </div>
         </div>
       </main>
     );
