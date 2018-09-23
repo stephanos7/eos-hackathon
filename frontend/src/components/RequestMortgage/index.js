b5fd4be386d5943a45ab80eba005ffa73ee89e89
hamid.adelyar <hamidadelyar@gmail.com>
2018-09-23 12:24:01 +0100

    Added modal

--- frontend/src/components/RequestMortgage/index.js
+++ frontend/src/components/RequestMortgage/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import "./styles.css";
-import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+const styles = theme => ({
+  textField: {}
+});
 
 class RequestMortgage extends Component {
+  state = {
+    openModal: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ openModal: true });
+  };
+
+  handleClose = () => {
+    this.setState({ openModal: false });
+  };
+
   render() {
     return (
       <main className="vertical-center">
@@ -19,18 +41,57 @@ class RequestMortgage extends Component {
             label="Passport"
           />
           <FormControlLabel
-            control={<Switch value="checkedB" color="primary" />}
+            control={<Switch value="checkedB" />}
             label="Bank Statement"
           />
           <FormControlLabel
             control={<Switch value="checkedC" />}
-            label="Uncontrolled"
+            label="Employment Letter"
           />
           <FormControlLabel
             control={<Switch value="checkedD" />}
-            label="Disabled"
+            label="Proof of Address"
           />
         </FormGroup>
+
+        <TextField
+          id="standard-bare"
+          className={styles.textField}
+          defaultValue="Account Name"
+        />
+
+        <br />
+        <br />
+        <Button
+          onClick={this.handleClickOpen}
+          variant="contained"
+          className="buttons"
+        >
+          Request
+        </Button>
+
+        <Dialog
+          open={this.state.openModal}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Request Sent"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Request has been sent, please wait until the user gives you
+              permission
+              <Button
+                component={Link}
+                to="/requestor"
+                variant="contained"
+                className="buttons"
+              >
+                Request
+              </Button>
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
       </main>
     );
   }
